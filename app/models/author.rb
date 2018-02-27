68aac4736f20e5a483335d3e4e68d2986d8bdc22
Nolan Byrnes <nolanehs@gmail.com>
2018-02-27 17:06:15 +0000

    automatically backed up by learn

---
+++ app/models/author.rb
@@ -0,0 +1,9 @@
+class Author < ActiveRecord::Base
+  validates :title, presence:true
+  validates :content, length: {minimum: 100}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+end
+
+title is not blank
+content is at least 100 characters long
+category is either "Fiction" or "Non-Fiction"
